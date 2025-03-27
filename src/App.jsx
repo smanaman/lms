20a69118200e4b035befc6213e5be9f4c6a9be43
smanaman <sanvatsman@gmail.com>
2025-03-27 12:47:09 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import Button from './Pages/Button'
-<<<<<<< HEAD
 import Aman from './components/Aman'
-=======
 import Button2 from './Button2'
-<<<<<<< HEAD
->>>>>>> 54a36a8f177046466465262c5f8557c97a6afe9d
-=======
+
 import Button3 from './Button3'
->>>>>>> fe3f90e59b7540d090abfd24aec8af84d338a203
 
 const App = () => {
   return (
