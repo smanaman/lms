381d83195ed8d296c09598ddda5d72d398690385
bhavinparmar005 <bhavinparmar005@gmail.com>
2025-03-27 12:30:09 -0700

    make a button 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from './Pages/Button'
+import Button2 from './Button2'
 
 const App = () => {
   return (
     <div>
       <Button/>
+      <Button2/>
     </div>
   )
 }
---
+++ src/Button2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button2() {
+  return (
+    <div>
+        <button>Bhavin</button>
+    </div>
+  )
+}
+
+export default Button2
